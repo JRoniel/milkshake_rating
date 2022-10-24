7477fa40231b398185cc9114b17a54dc7573dbb5
John Roniel <johnroniel8264@gmail.com>
2022-10-24 20:53:35 -0300

    echo "# Milkshake_Rating" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/JRoniel/Milkshake_Rating.git git push -u origin main

---
+++ controller/controller.js
@@ -0,0 +1,55 @@
+this.disable_submit();
+
+// Ativando botao
+function enable_submit() {
+    $("button.submit").disabled = false;
+    $("button.submit").addClass("not-disabled");
+}
+
+// Disativa botao
+function disable_submit() {
+    $("button.submit").disabled = true;
+    $("button.submit").removeClass("not-disabled");
+    $("button.submit").css("cursor", "default");
+}
+
+// Mostrando quadro de feedback apos voto
+$(".rating__input").on("click", function () {
+    var rating = this["value"];
+    $(".rating__label").removeClass("active");
+    $(this).siblings(".rating__label").addClass("active");
+    $(".feedback").css("display", "block");
+
+    rating_validate(rating);
+});
+
+//Pega atributo de um elemento pelo id.
+function getElement(data) {
+    return document.getElementById(data);
+}
+
+//Capturando dados do formulario e enviando para destino (console)
+function salveForm() {
+    let data = new Object();
+    data.voto = $("input[name=rating]").filter(":checked").val();
+    data.comentario = getElement("comentario").value;
+    data.nome = getElement("nome").value == "" ? "Usuário" : "";
+    data.telefone = getElement("telefone").value;
+
+    if (data.voto != undefined) {
+        console.log(data);
+        alert;
+    } else {
+        console.error("[01] PARAMENTRO RATING NÃO RECEBIDO");
+    }
+}
+
+// Run enable button function based on input
+$(".feedback textarea").keyup(function () {
+    if ($(".feedback textarea").val().length > 3) enable_submit();
+});
+
+// Enable or disable button by validation
+function rating_validate(val) {
+    val <= 3 ? disable_submit() & $("#negative").css("display", "block") : enable_submit() & $("#negative").css("display", "none");
+}
